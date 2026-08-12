cd0b100c5a48132afa013d186d3cca30b4b8b00f
Gabriel L. <gabrielliendo1257@gmail.com>
2026-08-12 09:55:09 -0500

    feat: telescope + fzf-native + ui-select para vim.ui

---
+++ lua/plugins/telescope.lua
@@ -0,0 +1,40 @@
+return {
+  {
+    "nvim-telescope/telescope.nvim",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      {
+        "nvim-telescope/telescope-fzf-native.nvim",
+        build = "make",
+      },
+      "nvim-telescope/telescope-ui-select.nvim",
+    },
+    cmd = "Telescope",
+    keys = {
+      { "<leader>ff", "<cmd>Telescope find_files<CR>", desc = "Find files" },
+      { "<leader>fg", "<cmd>Telescope live_grep<CR>", desc = "Live grep" },
+      { "<leader>fb", "<cmd>Telescope buffers<CR>", desc = "Buffers" },
+      { "<leader>fr", "<cmd>Telescope oldfiles<CR>", desc = "Recent files" },
+      { "<leader>fh", "<cmd>Telescope help_tags<CR>", desc = "Help tags" },
+      { "<leader>fk", "<cmd>Telescope keymaps<CR>", desc = "Keymaps" },
+    },
+    opts = function()
+      return {
+        defaults = {
+          sorting_strategy = "ascending",
+          layout_config = { prompt_position = "top" },
+          file_ignore_patterns = { "^%.git/", "node_modules/", ".venv/", "%.lock$" },
+        },
+        extensions = {
+          ["ui-select"] = require("telescope.themes").get_dropdown {},
+          fzf = {},
+        },
+      }
+    end,
+    config = function(_, opts)
+      require("telescope").setup(opts)
+      require("telescope").load_extension("fzf")
+      require("telescope").load_extension("ui-select")
+    end,
+  },
+}
