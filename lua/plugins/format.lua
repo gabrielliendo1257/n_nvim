f0f0fef7bb44e164a2ce23047101aa866be864d7
Gabriel L. <gabrielliendo1257@gmail.com>
2026-08-12 19:11:42 -0500

    feat: autopairs + formateo con conform (keymap <leader>cf)

---
+++ lua/plugins/format.lua
@@ -0,0 +1,22 @@
+return {
+  {
+    "stevearc/conform.nvim",
+    event = { "BufReadPre", "BufNewFile" },
+    opts = {
+      formatters_by_ft = {
+        lua = { "stylua" },
+        python = { "ruff_format" },
+      },
+      format_on_save = false,
+    },
+    keys = {
+      {
+        "<leader>cf",
+        function()
+          require("conform").format({ async = true, lsp_format = "fallback" })
+        end,
+        desc = "Format buffer (conform)",
+      },
+    },
+  },
+}
