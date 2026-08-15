5b719a258403725735db616b5763735d4eec5d83
Gabriel L. <gabrielliendo1257@gmail.com>
2026-08-15 18:02:08 -0500

    feat: navegacion entre ventanas con Ctrl+h/j/k/l

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -18,6 +18,11 @@ map({ "n", "v" }, "<leader>p", [["+p]], { desc = "Paste from system clipboard" }
 map("n", "<C-d>", "<C-d>zz", { desc = "Scroll down and center" })
 map("n", "<C-u>", "<C-u>zz", { desc = "Scroll up and center" })
 
+map("n", "<C-h>", "<C-w>h", { desc = "Window left" })
+map("n", "<C-j>", "<C-w>j", { desc = "Window down" })
+map("n", "<C-k>", "<C-w>k", { desc = "Window up" })
+map("n", "<C-l>", "<C-w>l", { desc = "Window right" })
+
 map("n", "<Esc>", "<cmd>nohlsearch<CR>", { desc = "Clear search highlight" })
 
 map("n", "<leader>th", function() require("config.theme").select() end, { desc = "Switch theme" })
